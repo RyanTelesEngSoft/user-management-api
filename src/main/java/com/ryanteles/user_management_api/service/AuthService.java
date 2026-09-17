1856b96f320c3d5ed5f999fde035f53417bb0d6c
Ryan Teles <ryanteles2018@gmail.com>
2026-09-17 20:56:00 -0300

    refactor: reorganiza AuthService para usar UsuarioRepository

--- src/main/java/com/ryanteles/user_management_api/service/AuthService.java
+++ src/main/java/com/ryanteles/user_management_api/service/AuthService.java
@@ -2,21 +2,20 @@ package com.ryanteles.user_management_api.service;
 
 import com.ryanteles.user_management_api.dto.LoginRequest;
 import com.ryanteles.user_management_api.entity.Usuario;
+import com.ryanteles.user_management_api.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-    private final UsuarioService usuarioService;
+    private final UsuarioRepository usuarioRepository;
 
-    public AuthService(UsuarioService usuarioService) {
-
-        this.usuarioService =
-                usuarioService;
+    public AuthService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
     }
 
     public String login(LoginRequest login) {
 
-        for (Usuario usuario : usuarioService.listarUsuarios()) {
+        for (Usuario usuario : usuarioRepository.findAll()) {
 
             if (usuario.getEmail().equals(login.getEmail()) && usuario.getSenha().equals(login.getSenha())) {
 
